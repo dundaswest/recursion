1813f7539c24ba4e95787aad87802edee32af5a1
Eunbyul Cho <hello@eunbyulcho.me>
2018-04-02 21:14:41 +0900

    finish stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,51 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+  let output = '';
+  if(typeof obj === 'number') {
+  	return `${obj}`;
+  } 
+  if(obj === null) {
+  	return `null`;
+  }
+  if(typeof obj === 'boolean') {
+  	return `${obj}`;
+  }
+  if(typeof obj === 'string') {
+  	return `"${obj}"`;
+  }
+  if(Array.isArray(obj)) {
+  	output = '[';
+  	if(obj.length < 1){
+  	return "[]";
+  	} else {
+  		for(let i = 0; i < obj.length;i++) {
+  			if(i === obj.length-1) {
+  				output+= `${stringifyJSON(obj[i])}`;
+  			} else {
+  				output+= `${stringifyJSON(obj[i])},`;
+  			}
+  		}
+  		return output + "]";
+  	}
+  }
+  if(typeof obj === 'object') {
+  	output = '{';
+  	let keyArr = Object.keys(obj);
+  	if(keyArr.length === 0) {
+  		return '{}';
+  	} else {
+  		for(let i = 0; i < keyArr.length;i++) {
+  			if(keyArr[i] === 'functions' || keyArr[i] === 'undefined') {
+  				return '{}';
+  			}
+  			if(i === keyArr.length-1) {
+  				output+= `${stringifyJSON(keyArr[i])}:${stringifyJSON(obj[keyArr[i]])}`;
+  			} else {
+  			output+= `${stringifyJSON(keyArr[i])}:${stringifyJSON(obj[keyArr[i]])},`;
+  			}
+  		}
+  		return output + '}';
+  	}
+  }
+ };
